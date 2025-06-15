02a801577ba2dd835909451664eb51d70ef7b31f
Ezebuike Michael <ezebuikemichael@gmail.com>
2025-06-15 19:48:03 +0100

    Fix bacckend

--- module/backend.tf
+++ module/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "teera-tf" # Replace with your bucket name
-    key            = "prometheus/terraform.tfstate"
-    region         = "eu-north-1"
-    encrypt        = true
+    bucket  = "teera-tf" # Replace with your bucket name
+    key     = "prometheus/terraform.tfstate"
+    region  = "eu-north-1"
+    encrypt = true
   }
 }
