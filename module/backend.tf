b430e4523eb9b6d6cd9b6f1278cf4db59e2887f1
Ezebuike Michael <ezebuikemichael@gmail.com>
2025-06-15 19:25:05 +0100

    Fix bacckend

--- module/backend.tf
+++ module/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "teera-tf"  # Replace with your bucket name
-    key            = "prometheus/terraform.tfstate"
-    region         = "eu-north-1"
-    encrypt        = true
-    dynamodb_table = "terraform-locks"  # Optional: for state locking
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "teera-tf"  # Replace with your bucket name
+#     key            = "prometheus/terraform.tfstate"
+#     region         = "eu-north-1"
+#     encrypt        = true
+#     dynamodb_table = "terraform-locks"  # Optional: for state locking
+#   }
+# }
