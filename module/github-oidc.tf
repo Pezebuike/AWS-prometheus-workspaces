7d21491bc101b291d51fec323b64a2173577779e
Ezebuike Michael <ezebuikemichael@gmail.com>
2025-06-17 08:00:14 +0100

    removed github-oidec.tf

--- module/github-oidc.tf
+++ module/github-oidc.tf
@@ -1,23 +1,23 @@
-# GitHub OIDC Identity Provider
-resource "aws_iam_openid_connect_provider" "github_actions" {
-  count = var.create_oidc_role ? 1 : 0
+# # GitHub OIDC Identity Provider
+# resource "aws_iam_openid_connect_provider" "github_actions" {
+#   count = var.create_oidc_role ? 1 : 0
 
-  url = "https://token.actions.githubusercontent.com"
+#   url = "https://token.actions.githubusercontent.com"
 
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
+#   client_id_list = [
+#     "sts.amazonaws.com"
+#   ]
 
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
-  ]
+#   thumbprint_list = [
+#     "6938fd4d98bab03faadb97b34396831e3780aea1"
+#   ]
 
-  tags = {
-    Name       = "github-actions-oidc"
-    Purpose    = "GitHub Actions OIDC authentication"
-    Repository = "${var.github_username}/${var.github_repository}"
-  }
-}
+#   tags = {
+#     Name       = "github-actions-oidc"
+#     Purpose    = "GitHub Actions OIDC authentication"
+#     Repository = "${var.github_username}/${var.github_repository}"
+#   }
+# }
 
 
 
